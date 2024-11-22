0895bd119cd68f7669385a2a4153ad6710101b9f
xjh521 <1632590674@qq.com>
2024-11-22 11:27:00 +0800

    Update conf_mysql.go

--- config/conf_mysql.go
+++ config/conf_mysql.go
@@ -9,8 +9,6 @@ type Mysql struct {
 	DB           string `yaml:"db"`     //数据库名
 	User         string `yaml:"user"`   //数据库用户名
 	Password     string `yaml:"password"`
-	MaxIdleConns int    `json:"max_idle_conns" yaml:"max_idle_conns"` //空闲着中的最大连接数
-	MaxOpenConns int    `json:"max_open_conns" yaml:"max_open_conns"` //打开到数据库的最大连接数
 	LogLevel     string `yaml:"logLevel"`                             //日志等级  debug是输出全部sql，dev，release
 }
 
